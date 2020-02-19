bf6aa68f757e151e90ab705c5f963916a613a933
Ed Chen <aedyucheng@gmail.com>
2020-02-19 23:39:26 +0800

    Add the order page

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { withRouter} from 'react-router-dom';
 
 import Button from '../../../components/UI/Button/Button';
 import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/spinner/Spinner';
 
 import classes from './ContactData.module.css'
 
@@ -20,7 +22,7 @@ class ContactData extends Component {
         event.preventDefault();
         this.setState({ loading: true })
         const order = {
-            ingreients: this.props.ingredients,
+            ingredients: this.props.ingredients,
             price: this.props.price,
             customer: {
                 name: 'Ed Chen',
@@ -36,6 +38,7 @@ class ContactData extends Component {
         axios.post('/orders.json', order)
             .then(response => {
                 this.setState({ loading: false });
+                this.props.history.push('/');
             })
             .catch(error => {
                 this.setState({ loading: false });
@@ -43,19 +46,26 @@ class ContactData extends Component {
     }
 
     render() {
+        let form = (
+            <form>
+                <input className={classes.Input} type="text" name="name" placeholder="Your name" />
+                <input className={classes.Input} type="email" name="emal" placeholder="Your email" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="text" name="postal" placeholder="Postal code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
         return (
             <div className={classes.ContactData}>
                 <h4>Enter Your Contact Data</h4>
-                <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-                    <input className={classes.Input} type="email" name="emal" placeholder="Your email" />
-                    <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal code" />
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
 }
 
-export default ContactData;
+export default withRouter(ContactData);
