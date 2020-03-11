d0985480df0ef1210de38944c3d6b81183bbcb71
Ed Chen <aedyucheng@gmail.com>
2020-03-11 11:26:50 +0800

    Add authentication

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,41 @@
+import * as actions from '../actions/actionTypes';
+import { updateObject } from '../Utility';
+
+const initState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: false
+}
+
+const reducer = (state = initState, action) => {
+    switch (action.type) {
+        case actions.AUTH_START: return authStart(state, action);
+        case actions.AUTH_SUCCESS: return authSuccess(state, action);
+        case actions.AUTH_FAIL: return authFail(state, action);
+        default: return state;
+    }
+}
+
+const authStart = (state, action) => {
+    return updateObject(state, { error: null, loading: true });
+}
+
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        token: action.token,
+        userId: action.userId,
+        error: null,
+        loading: false,
+    });
+}
+
+const authFail = (state, action) => {
+    return updateObject(state, { 
+        error: action.error,
+        loading: false 
+    });
+}
+
+export default reducer;
+
